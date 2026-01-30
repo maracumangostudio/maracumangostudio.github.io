b3616270c0c39dd08c66be3188550aff799e58c5
Gerardo871 <gerardogomero871@gmail.com>
2026-01-30 13:17:04 -0500

    second

--- asset/script.js
+++ asset/script.js
@@ -2,4 +2,6 @@ const hamburger = document.getElementById('hamburger');
 const navLinks = document.getElementById('navLinks');
 hamburger.addEventListener('click', () => {
     navLinks.classList.toggle('show');
+    hamburger.classList.toggle("active");
+    navLinks.classList.toggle("active");
 });
